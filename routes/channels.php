c31a1b939130395ebcc9f5176f8380cdcc4f2307
Annurman <150548765+Annurman@users.noreply.github.com>
2025-11-27 13:41:20 +0700

    Comment out broadcast channel definitions
    Comment out admin and user broadcast channel definitions.

--- routes/channels.php
+++ routes/channels.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Auth;
 
-Broadcast::channel('admin.{adminId}', function ($admin, $adminId) {
-    return Auth::guard('admin')->check() && Auth::guard('admin')->id() == (int) $adminId;
-});
+// Broadcast::channel('admin.{adminId}', function ($admin, $adminId) {
+//     return Auth::guard('admin')->check() && Auth::guard('admin')->id() == (int) $adminId;
+// });
 
 
-Broadcast::channel('user.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+// Broadcast::channel('user.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
