08f87539d9009421c234c7e60b871094cf720062
Annurman <150548765+Annurman@users.noreply.github.com>
2025-11-29 09:52:16 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,9 @@
         <!-- Logo Pinlib -->
         <div class="flex justify-center">
             <a href="/">
-                <img src="{{ asset('images/logo-pinlib.png') }}" alt="Logo Pinlib" class="h-[170px] w-auto">
+                <img src="{{ asset('images/loo biu.png') }}" alt="Logo Pinlib" class="h-[170px] w-auto">
             </a>
         </div>
-
         <div class="w-full sm:max-w-md mt-6 px-6 py-8 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
