e738ecb3e688e08bae4770775c6a72a693f805bb
Annurman <150548765+Annurman@users.noreply.github.com>
2025-11-29 08:57:47 +0700

    upadate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+    }
+
+    public function boot(): void
+    {
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 
     /**
